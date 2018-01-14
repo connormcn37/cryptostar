77d8370be53731e114e9f4a34073680be8e33d9e
connor <connormcn37@gmail.com>
2018-01-14 10:33:53 -0800

    minor changes to contract

--- contracts/starfactory.sol
+++ contracts/starfactory.sol
@@ -4,11 +4,17 @@ import "./ownable.sol";
 
 contract StarFactory is Ownable {
 
+    enum Race { Organic, Inorganic, Energy, Empty };
+
     struct Star {
         string name;
         uint latitude;
         uint longitude;
+        uint minerals;
     }
+
+    //uint dnaDigits = 16;
+    //uint dnaModulus = 10 ** dnaDigits;
 
     event NewStar(id, _name, _latitude, _longitude);
 
@@ -17,17 +23,21 @@ contract StarFactory is Ownable {
     // mapping is like a dictionary, and address an eth address type
     mapping (uint => address) public starToOwner;
     mapping (address => uint) ownerStarCount;
-
+    //mapping (address => Race)
     // note: external (not used here) can only be called outside the contract
-    function createStar(string _name, uint _latitude, uint _longitude) onlyOwner {
+    function createStar(string _name, uint _latitude, uint _longitude, _minerals, _natives) onlyOwner {
         uint id = stars.push(Star(_name, _dna, _longitude);
         // msg.sender is the address of the caller
         starToOwner[id] = msg.sender;
         ownerStarCount[msg.sender]++;
         // call the event, so the listeners will get the notice of a new created star
-        NewStar(id, _name, _latitude, _longitude);
+        NewStar(id, _name, _latitude, _longitude,);
     }
-
+    /*
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }*/
     // view functions can access but not change storage variables
     // note: pure (not used here) is all self contained and only uses the parameters
 
